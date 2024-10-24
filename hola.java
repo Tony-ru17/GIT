04984b8abc0f0c20b5641cddc4c7b6176baffe56
Tony <ciclosm@localhost>
2024-10-24 12:40:21 +0200

    hola.java creado

---
+++ hola.java
@@ -0,0 +1,9 @@
+public class hola {
+
+    public static void main(String[] args) {
+     
+        System.out.println("Hola Enric, te compro a Lewan");
+        System.out.println("Adiós, muchas gracias.");
+    }
+
+}
